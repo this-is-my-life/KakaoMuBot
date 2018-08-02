1330521451c0766cb19d9a9b40812b821b28a5b4
PMHStudio <kok4575@gmail.com>
2018-08-02 13:22:18 +0900

    [08/02] Basic Updated

--- CORE.js
+++ CORE.js
@@ -1,13 +1,56 @@
+/*
+	μBot k1.0 Core. 
+	-------------------------
+	PMH Studio / Proj- μBot | Smart & Cute Kakao Bot_Mu~☆ 
+	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+	
+	* Requests Node.js & Discord.js
+*/
+
 var API = require('express');
 var http = require('http')
+var bodyParser = require('body-parser');
 var mu = API();
+
+mu.use(bodyParser.urlencoded({extended: false}));
+mu.use(bodyParser.json());
 
 mu.get('/keyboard', function(req, res) {
     var data = {
         "type" : "buttons",
-        "buttons" : ["선택 1", "선택 2", "선택 3"]
+        "buttons" : ["> Start μBot"]
       };
     res.json(data);
+});
+
+mu.post('/message', function(req, res) {
+    var input = req.body.content;
+    console.log('Input: '+ input);
+    var send ={};
+    switch (input) {
+
+        // Commands ...................
+
+        case '> Start μBot':
+            send = {
+                'message': {
+                    'text': 'ㅎㅇ뮤~★'
+                }
+            }
+            break;
+
+        default:
+            send = {
+                'message': {
+                    'text': '뮤가 알지못하는 단어다뮤~!'
+                }
+            }
+            break;
+
+        // ............................
+
+    };
+    res.json(send);
 });
 
 http.createServer(mu).listen(8080, function() {
